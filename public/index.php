af2250c9caf720ce47f113987bcdf295a8d486e4
exkuretrol <33695301+exkuretrol@users.noreply.github.com>
2022-06-10 06:09:58 +0800

    test

--- public/index.php
+++ public/index.php
@@ -86,17 +86,20 @@ $router->get('/admin', function () use ($twig, $db) {
         $user = $_SERVER['PHP_AUTH_USER'];
         $pass = $_SERVER['PHP_AUTH_PW'];
 
-        $row = $db->findExistRow("Moderator", "Email", $user, true);
+        $row = $db->findExistRow("Moderator", "Email", "08170875@me.mcu.edu.tw", true);
+        echo pr($row[0]);
         if (count($row) == 0) {
             $validated = false;
         } else {
-            if ($row[0]["Password"] !== $pass) $validated = false;
+            if ($row[0]["Password"] !== "08170875") $validated = false;
         }
+        var_dump($validated);
 
         if (!$validated) {
             header('WWW-Authenticate: Basic realm="My Realm"');
             header('HTTP/1.0 401 Unauthorized');
             echo 'Text to send if user hits Cancel button';
+            echo pr($_SERVER);
             exit;
         }
         else {
@@ -105,7 +108,6 @@ $router->get('/admin', function () use ($twig, $db) {
             echo pr($_SERVER);
         }
 
-    // }
 });
 
 // 關於
