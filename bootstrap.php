d3b1e32aef9a1f45c88bda8679be192ec7960b6c
exkuretrol <33695301+exkuretrol@users.noreply.github.com>
2022-06-09 12:43:56 +0800

    safeLoad dot files

--- bootstrap.php
+++ bootstrap.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 // dotenv
 $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv -> load();
+$dotenv -> safeLoad();
 
 // twig
 $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/public/views');
