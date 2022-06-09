517b0b2612227c7ee0cb806425c91f595184acea
exkuretrol <33695301+exkuretrol@users.noreply.github.com>
2022-06-10 04:05:18 +0800

    add monolog package

--- bootstrap.php
+++ bootstrap.php
@@ -3,6 +3,14 @@ session_start();
 // Load our autoloader
 require_once __DIR__ . '/vendor/autoload.php';
 
+use Monolog\Level;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// create a log channel
+$log = new Logger('Debug');
+$log->pushHandler(new StreamHandler('php://stdout', Level::Warning));
+
 // dotenv
 $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv -> safeLoad();
