1add1c3df1b237065d989a30d87da679715b3fbc
exkuretrol <33695301+exkuretrol@users.noreply.github.com>
2022-06-10 05:07:36 +0800

    test

--- bootstrap.php
+++ bootstrap.php
@@ -3,9 +3,9 @@ session_start();
 // Load our autoloader
 require_once __DIR__ . '/vendor/autoload.php';
 
+use Monolog\Handler\StreamHandler;
 use Monolog\Level;
 use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
 
 // create a log channel
 $log = new Logger('Debug');
@@ -13,7 +13,7 @@ $log->pushHandler(new StreamHandler('php://stdout', Level::Warning));
 
 // dotenv
 $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv -> safeLoad();
+$dotenv->safeLoad();
 
 // twig
 $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/public/views');
